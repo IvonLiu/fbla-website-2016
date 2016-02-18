803a1d7aef69c1ec8aa3d60e66b2c2270920afd2
Ivon Liu <ivon.liu2011@gmail.com>
2016-02-18 01:59:01 -0800

    Ugly reviews

--- js/controllers/reviewsCtrl.js
+++ js/controllers/reviewsCtrl.js
@@ -1,7 +1,21 @@
 app.controller('ReviewsCtrl', [
   '$scope',
   'utils',
-  function($scope, utils) {
+  'reviews',
+  function($scope, utils, reviews) {
 
+    $scope.reviews = reviews;
+
+    $scope.addReview = function() {
+      $scope.reviews.$add({
+        timestamp: moment().format(),
+        email: $scope.email,
+        name: $scope.name,
+        content: $scope.content
+      });
+      $scope.email = '';
+      $scope.name = '';
+      $scope.content = '';
+    }
   }
 ]);
